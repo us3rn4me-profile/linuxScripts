ac0d6da0eb513ed1cc48d1b0546f62c190384dcf
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-08-07 18:52:50 +0300

    Some changes in converter script

--- mp4_to_mp3.sh
+++ mp4_to_mp3.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-MP4F=$(ls ~/Music/ | grep .mp4)
-for filename in $MP4F
-do 
- name=`echo "$filename" | sed -e "s/.mp4$//g"`
- ffmpeg -i ~/Music/$filename -b:a 192K -vn ~/Music/$name.mp3
+directory=$(pwd])
+MP4=$(ls $directory | grep .mp4)
+for filename in $MP4
+do
+  name=`echo "$filename" | sed -e "s/.mp4$//g"`
+  ffmpeg -i ~/$directory/$filename -b:a 192K -vn ~/$directory/$name.mp3
 done
